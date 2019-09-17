a70acc68675ec3d1d94abccbb92d8b69157e0da4
Gerardo Gabriel Rosas Rodríguez <grosas@gdc.ad.com.mx>
2019-09-17 12:22:13 -0500

    correcion del index

--- routes/index.js
+++ routes/index.js
@@ -5,38 +5,34 @@ const puppeteer = require('puppeteer');
 
 
 router.post('/gpdf', async (req, res) => {
-    const {nombre, url } = req.body
-     
-        //pool.query('INSERT INTO documentos set ?', [nuevo_doc]);
-        //-----la linea de abajo genera el pdf
-        try {
-           // const direfile =  path.join('./documentos/plantillas/'+ plantilla);
+    const {nombre, url } = req.body;
+    console.log('nombre :' + nombre);
+    console.log('url :' + url);
        
-       // let archivo = fs.readFileSync(direfile, 'utf-8');
-        //console.log(archivo);
+        try {
+          
             const browser = await puppeteer.launch();
             const page = await browser.newPage();  
-          //generar pdf por contenido
-           // await page.setContent(archivo);
-          //generar pdf por url;
+          
           await page.goto(url , { waitUntil: 'networkidle0' });
             await page.emulateMedia('screen');
             await page.pdf({
-                path: './' + nombre + '.pdf',
+                path: './documentos/' + nombre + '.pdf',
                 format: 'A4',
                 printBackground: true
             });
+            
             console.log('documento creado');
             await browser.close();
-           // process.exit();
+         
            console.log('el documento se genero')
 
-            res.json({'url': './' + nombre });
+            res.json({'urlgenerada': './documentos/' + nombre, 'urlmandad': url });
 
         } catch (error) {
             console.log('ocurrio un error'+ error);
-           console.log('el documento no se puedo generar');
-            res.json({'error': './' + nombre });
+           
+	res.json({'error': error,'urlgenerada': './documentos/' + nombre, 'urlmandad': url });
         }
 });
 
